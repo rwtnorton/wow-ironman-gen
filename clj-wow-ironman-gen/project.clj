e412ff0add84bf89ecbb4ade662a706c6bdcfb68
Richard W. Norton <rwtnorton@gmail.com>
2013-01-08 22:41:55 -0600

    Added lein-generated Clojure version.

---
+++ clj-wow-ironman-gen/project.clj
@@ -0,0 +1,8 @@
+(defproject clj-wow-ironman-gen "0.1.0-SNAPSHOT"
+  :description "WoW Ironman Char Generator"
+  :url "https://github.com/rwtnorton/wow-ironman-gen/clj_wow_ironman_gen"
+  :license {:name "GNU Public License v3"
+            :url "https://www.gnu.org/licenses/gpl"}
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :plugins [[lein-swank "1.4.4"]]
+  :main clj-wow-ironman-gen.core)
---
+++ clj-wow-ironman-gen/test/clj_wow_ironman_gen/core_test.clj
@@ -0,0 +1,48 @@
+(ns clj-wow-ironman-gen.core-test
+  (:use clojure.test
+        clj-wow-ironman-gen.core))
+
+(deftest races-test
+  (testing "races are sane"
+    (is (= (set (keys classes-for-race))
+           (set (races))))))
+
+(deftest cls-test
+  (testing "cls has reasonable results"
+    (is (contains? (set (classes-for-race :gnome))
+                   (cls :gnome)))))
+
+(deftest no-gnome-druid-test
+  (testing "gnomes cannot be druids, evar"
+    (is (not (contains? (set (classes-for-race :gnome))
+                        :druid)))))
+
+(deftest gender-test
+  (testing "gender is female or male"
+    (let [g (gender)]
+      (is (or (= g :female)
+              (= g :male))))))
+
+(deftest faction-test
+  (testing "faction is horde or ... alliance, psshh"
+    (let [f (faction)]
+      (is (or (= f :horde)
+              (= f :alliance))))))
+
+(deftest pandaren-present-toon-has-faction
+  (testing "pandaren toons should have faction in present-toon"
+    (let [t {:race :pandaren,
+             :cls :monk,
+             :gender :female,
+             :faction :horde}]
+      (is (= (present-toon t)
+             "female horde pandaren monk")))))
+
+(deftest non-pandaren-present-toon-lacks-faction
+  (testing "non-pandaren toons should lack faction in present-toon"
+    (let [t {:race :tauren,
+             :cls :hunter,
+             :gender :female,
+             :faction :horde}]
+      (is (= (present-toon t)
+             "female tauren hunter")))))
