a8ee581aaf9b5a06c9b09b67bc76846145b89c1c
Richard W. Norton <rwtnorton@gmail.com>
2013-05-26 06:01:34 -0500

    Adjusted standalone clj.
    Incorporated recent lein project changes.
    
    Following the advice on http://clojure.org/compilation,
    specifically :gen-class and (def -main ...), to have
    something that clojure.main can compile directly.  Also,
    had to use underscores in filename (boo).

---
+++ wow_ironman_gen.clj
@@ -0,0 +1,60 @@
+;;
+;; Randomly generate a World of Warcraft race/class/gender combo for the
+;; WoW Ironman Challenge.
+;;
+
+;; human, night_elf, dwarf, gnome, draenei, worgen, pandaren,
+;;  orc, troll, forsaken, tauren, blood_elf, goblin
+;; No death knights.
+;; warrior, paladin, hunter, shaman, druid, rogue, monk, mage, warlock, priest
+
+(ns wow-ironman-gen
+  (:gen-class))
+
+(defn sample [coll]
+  (rand-nth (seq coll)))
+
+(def classes-for-race
+  {
+   :human #{:warrior :paladin :hunter :rogue :monk :mage :warlock :priest},
+   :night_elf #{:warrior :hunter :rogue :druid :monk :mage :priest},
+   :dwarf #{:warrior :paladin :hunter :shaman :rogue :monk :mage :warlock
+            :priest},
+   :gnome #{:warrior :rogue :monk :mage :warlock :priest}
+   :draenei #{:warrior :paladin :hunter :shaman :monk :mage :priest},
+   :worgen #{:warrior :hunter :druid :rogue :mage :warlock :priest},
+   :pandaren #{:warrior :hunter :shaman :rogue :monk :mage :priest},
+   :orc #{:warrior :hunter :shaman :rogue :monk :mage :warlock},
+   :troll #{:warrior :hunter :shaman :druid :rogue :monk :mage :warlock
+            :priest},
+   :forsaken #{:warrior :hunter :rogue :monk :mage :warlock :priest},
+   :tauren #{:warrior :paladin :hunter :shaman :druid :monk :priest},
+   :blood_elf #{:warrior :paladin :hunter :rogue :monk :mage :warlock :priest},
+   :goblin #{:warrior :hunter :shaman :rogue :mage :warlock :priest},
+   })
+
+(defn races [] (keys classes-for-race))
+
+(defn race [] (sample (races)))
+
+(defn cls [race] (sample (classes-for-race race)))
+
+(defn gender [] (sample #{:female :male}))
+
+(defn faction [] (sample #{:horde :alliance}))
+
+(defn toon []
+  (let [r (race)
+        c (cls r)
+        g (gender)
+        f (faction)]
+    {:race r, :cls c, :gender g, :faction f}))
+
+(defn present-toon [toon]
+  (let [{r :race, c :cls, g :gender, f :faction} toon
+        attrs (if (= r :pandaren) (list g f r c) (list g r c))
+        attr->str #(subs (str %) 1)]
+    (clojure.string/join " " (map attr->str attrs))))
+
+(defn -main [& args]
+  (println (present-toon (toon))))
