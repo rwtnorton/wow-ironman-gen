9a2284c1d6f489836178c4be5a770cf4529d72b3
Richard W. Norton <rwtnorton@gmail.com>
2013-08-31 22:57:02 -0500

    Ditched wonky Samplable trait.
    More complicated than it was worth where a simple function works
    just as well.

--- scala_wow_ironman_gen/Main.scala
+++ scala_wow_ironman_gen/Main.scala
@@ -12,18 +12,14 @@ import scala.util.Random
 // No death knights.
 // warrior, paladin, hunter, shaman, druid, rogue, monk, mage, warlock, priest
 
-// Bleh, not working.  Resort to copy-and-paste FTW.  FUUUUUUUUUUUUU
-// trait Samplable[T] {
-//   def sample[T]: T = {
-//     Random.shuffle(values.toList).head
-//   }
-// }
+object Sampler {
+  def sample[T](vs: Seq[T]): T = Random.shuffle(vs).head
+}
 
 object Cls extends Enumeration {
   type Cls = Value
   val warrior, paladin, hunter, shaman, druid,
     rogue, monk, mage, warlock, priest = Value
-  def sample = Random.shuffle(values.toList).head
 }
 import Cls._
 
@@ -31,40 +27,38 @@ object Race extends Enumeration {
   type Race = Value
   val human, night_elf, dwarf, gnome, draenei, worgen, pandaren,
     orc, troll, forsaken, tauren, blood_elf, goblin = Value
-  def sample = Random.shuffle(values.toList).head
 }
 import Race._
 
 object Gender extends Enumeration {
   type Gender = Value
   val female, male = Value
-  def sample = Random.shuffle(values.toList).head
 }
 import Gender._
 
 object Faction extends Enumeration {
   type Faction = Value
   val horde, alliance = Value
-  def sample = Random.shuffle(values.toList).head
 }
 import Faction._
 
-object ClassesForRace {
+object ClsMapper {
   val classesForRace = Map(
-    human -> Set(warrior, paladin, hunter, rogue, monk, mage, warlock, priest),
+    human     -> Set(warrior, paladin, hunter, rogue, monk, mage, warlock, priest),
     night_elf -> Set(warrior, hunter, rogue, druid, monk, mage, priest),
-    dwarf -> Set(warrior, paladin, hunter, shaman, rogue, monk, mage, warlock, priest),
-    gnome -> Set(warrior, rogue, monk, mage, warlock, priest),
-    draenei -> Set(warrior, paladin, hunter, shaman, monk, mage, priest),
-    worgen -> Set(warrior, hunter, druid, rogue, mage, warlock, priest),
-    pandaren -> Set(warrior, hunter, shaman, rogue, monk, mage, priest),
-    orc -> Set(warrior, hunter, shaman, rogue, monk, mage, warlock),
-    troll -> Set(warrior, hunter, shaman, druid, rogue, monk, mage, warlock, priest),
-    forsaken -> Set(warrior, hunter, rogue, monk, mage, warlock, priest),
-    tauren -> Set(warrior, paladin, hunter, shaman, druid, monk, priest),
+    dwarf     -> Set(warrior, paladin, hunter, shaman, rogue, monk, mage, warlock, priest),
+    gnome     -> Set(warrior, rogue, monk, mage, warlock, priest),
+    draenei   -> Set(warrior, paladin, hunter, shaman, monk, mage, priest),
+    worgen    -> Set(warrior, hunter, druid, rogue, mage, warlock, priest),
+    pandaren  -> Set(warrior, hunter, shaman, rogue, monk, mage, priest),
+    orc       -> Set(warrior, hunter, shaman, rogue, monk, mage, warlock),
+    troll     -> Set(warrior, hunter, shaman, druid, rogue, monk, mage, warlock, priest),
+    forsaken  -> Set(warrior, hunter, rogue, monk, mage, warlock, priest),
+    tauren    -> Set(warrior, paladin, hunter, shaman, druid, monk, priest),
     blood_elf -> Set(warrior, paladin, hunter, rogue, monk, mage, warlock, priest),
-    goblin -> Set(warrior, hunter, shaman, rogue, mage, warlock, priest)
+    goblin    -> Set(warrior, hunter, shaman, rogue, mage, warlock, priest)
   )
+  def apply(r: Race) = classesForRace(r)
 }
 
 case class Toon(
@@ -75,26 +69,24 @@ case class Toon(
 
   val shouldShowFaction = race == pandaren
 
-  def showableAttrs(): Array[String] = {
+  val showableAttrs: Array[String] =
     if (shouldShowFaction)
       Array(gender, faction, race, cls) map (_.toString)
     else
       Array(gender, race, cls) map (_.toString)
-  }
 
   override def toString(): String = showableAttrs mkString " "
 }
 
 object Toon {
   def generateRandom: Toon = {
-    val r = Race.sample
-    val c = Random.shuffle(ClassesForRace.classesForRace(r).toList).head
-    val g = Gender.sample
-    val f = Faction.sample
+    val r = Sampler.sample(Race.values.toSeq)
+    val c = Sampler.sample(ClsMapper(r).toSeq)
+    val g = Sampler.sample(Gender.values.toSeq)
+    val f = Sampler.sample(Faction.values.toSeq)
     Toon(r, c, g, f)
   }
 }
-
 
 // $ cd path/to/wow_ironman_gen/dir
 // $ scalac scala_wow_ironman_gen/Main.scala
