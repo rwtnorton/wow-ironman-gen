0b23bda2dcee5bdf7463165c2ddc0b2f8f37af3a
Richard W. Norton <rwtnorton@gmail.com>
2015-01-31 16:27:35 -0800

    Added races module.
    This module and the genders module are *very* similar.
    Look for ways to consolidate.

---
+++ erlang/races.erl
@@ -0,0 +1,43 @@
+-module(races).
+-import(sample, []).
+-export([as_list/0, random/0, start/0, loop/0, rpc/2]).
+
+as_list() ->
+    [
+     human,
+     night_elf,
+     dwarf,
+     gnome,
+     draenei,
+     worgen,
+     pandaren,
+     orc,
+     troll,
+     forsaken,
+     tauren,
+     blood_elf,
+     goblin
+    ].
+
+random() ->
+    sample:from_list(as_list()).
+
+start() ->
+    spawn(races, loop, []).
+
+loop() ->
+    receive
+        {From, random} ->
+            From ! {self(), random()},
+            loop();
+        {From, all} ->
+            From ! {self(), as_list()},
+            loop()
+    end.
+
+rpc(Pid, Request) ->
+    Pid ! {self(), Request},
+    receive
+        {Pid, Response} ->
+            Response
+    end.
