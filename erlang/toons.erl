86157ea59502f85ead5466d5801f59261be4816f
Richard W. Norton <rwtnorton@gmail.com>
2015-01-31 23:10:38 -0800

    Switched toon from record to map.

--- erlang/toons.erl
+++ erlang/toons.erl
@@ -4,20 +4,32 @@
 -import(factions, []).
 -import(races, []).
 -import(classes, []).
--export([random/0, random/1, start/0, loop/0, rpc/2]).
-
--record(toon, {gender, faction, race, class}).
+-export([random/0, random/1, to_str/1, start/0, loop/0, rpc/2]).
 
 random(Race) ->
     Faction = factions:random(Race),
     Gender = genders:random(),
     Class = classes:random(Race),
-    #toon{gender=Gender
-         ,faction=Faction
-         ,race=Race
-         ,class=Class}.
+    #{gender  => Gender
+     ,faction => Faction
+     ,race    => Race
+     ,class   => Class}.
 random() ->
     random(races:random()).
+
+raw_to_str(Fmt, Args) ->
+    lists:flatten(io_lib:format(Fmt, Args)).
+
+to_str(#{class := Class
+        ,faction := Faction
+        ,gender := Gender
+        ,race := pandaren}) ->
+    raw_to_str("~p ~p ~p ~p", [Gender, Faction, pandaren, Class]);
+to_str(#{class := Class
+        ,faction := _
+        ,gender := Gender
+        ,race := Race}) ->
+    raw_to_str("~p ~p ~p", [Gender, Race, Class]).
 
 start() ->
     spawn(toons, loop, []).
