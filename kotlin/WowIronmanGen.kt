4ceb1d2a87479cc4dff49f91980c773005bb556d
Richard W. Norton <rwtnorton@gmail.com>
2018-12-15 00:11:03 -0800

    incorporated random attr generation

--- kotlin/WowIronmanGen.kt
+++ kotlin/WowIronmanGen.kt
@@ -1,5 +1,8 @@
 package wow_ironman_gen
 
+import kotlin.random.Random
+import kotlin.math.*
+
 //
 // Randomly generate a World of Warcraft race/class/gender combo for the
 // WoW Ironman Challenge.
@@ -34,10 +37,62 @@ fun main(args: Array<String>) {
         wowClass = WowClass.Hunter
     )
     println(toon3)
+    println(Race.values().toList())
+    for (i in 1..5) {
+        println("${Race.random()} ${Faction.random()} ${Gender.random()}")
+    }
+    println("========================")
+    repeat(10) {
+        println(Toon.random())
+    }
+    println("=======================")
+    val p = Race.Pandaren
+    repeat(5) {
+        println(p.faction)
+    }
+    println("=======================")
+    repeat(5) {
+        println(Race.Pandaren.faction)
+    }
+
+}
+
+val rand = Random(System.currentTimeMillis())
+
+fun <T> List<T>.sample(): T =
+    if (isEmpty())
+        throw IllegalStateException("cannot sample empty collection")
+    else
+        get((rand.nextInt() % size).absoluteValue)
+
+fun <T> Set<T>.sample(): T =
+    if (isEmpty())
+        throw IllegalStateException("cannot sample empty collection")
+    else
+        toList().sample()
+
+fun classesForRace(race: Race): Set<WowClass> = when (race) {
+    Race.Human -> setOf(WowClass.Warrior, WowClass.Paladin, WowClass.Hunter, WowClass.Rogue, WowClass.Monk, WowClass.Mage, WowClass.Warlock, WowClass.Priest)
+    Race.NightElf -> setOf(WowClass.Warrior, WowClass.Hunter, WowClass.Rogue, WowClass.Druid, WowClass.Monk, WowClass.Mage, WowClass.Priest)
+    Race.Dwarf -> setOf(WowClass.Warrior, WowClass.Paladin, WowClass.Hunter, WowClass.Shaman, WowClass.Rogue, WowClass.Monk, WowClass.Mage, WowClass.Warlock, WowClass.Priest)
+    Race.Gnome -> setOf(WowClass.Warrior, WowClass.Rogue, WowClass.Monk, WowClass.Mage, WowClass.Warlock, WowClass.Priest)
+    Race.Draenei -> setOf(WowClass.Warrior, WowClass.Paladin, WowClass.Hunter, WowClass.Shaman, WowClass.Monk, WowClass.Mage, WowClass.Priest)
+    Race.Worgen -> setOf(WowClass.Warrior, WowClass.Hunter, WowClass.Druid, WowClass.Rogue, WowClass.Mage, WowClass.Warlock, WowClass.Priest)
+    Race.Pandaren -> setOf(WowClass.Warrior, WowClass.Hunter, WowClass.Shaman, WowClass.Rogue, WowClass.Monk, WowClass.Mage, WowClass.Priest)
+    Race.Orc -> setOf(WowClass.Warrior, WowClass.Hunter, WowClass.Shaman, WowClass.Rogue, WowClass.Monk, WowClass.Mage, WowClass.Warlock)
+    Race.Troll -> setOf(WowClass.Warrior, WowClass.Hunter, WowClass.Shaman, WowClass.Druid, WowClass.Rogue, WowClass.Monk, WowClass.Mage, WowClass.Warlock, WowClass.Priest)
+    Race.Forsaken -> setOf(WowClass.Warrior, WowClass.Hunter, WowClass.Rogue, WowClass.Monk, WowClass.Mage, WowClass.Warlock, WowClass.Priest)
+    Race.Tauren -> setOf(WowClass.Warrior, WowClass.Paladin, WowClass.Hunter, WowClass.Shaman, WowClass.Druid, WowClass.Monk, WowClass.Priest)
+    Race.BloodElf -> setOf(WowClass.Warrior, WowClass.Paladin, WowClass.Hunter, WowClass.Rogue, WowClass.Monk, WowClass.Mage, WowClass.Warlock, WowClass.Priest)
+    Race.Goblin -> setOf(WowClass.Warrior, WowClass.Hunter, WowClass.Shaman, WowClass.Rogue, WowClass.Mage, WowClass.Warlock, WowClass.Priest)
 }
 
 enum class WowClass {
     Warrior, Paladin, Hunter, Shaman, Druid, Rogue, Monk, Mage, Warlock, Priest;
+
+    companion object {
+        fun randomForRace(race: Race) = classesForRace(race).sample()
+    }
 
     override fun toString() = when (this) {
         Warrior -> "warrior"
@@ -68,6 +123,20 @@ enum class Race {
     BloodElf,
     Goblin;
 
+    val isMultiFaction: Boolean
+        get() = this == Pandaren
+
+    val faction: Faction
+        get() = when (this) {
+            Human, NightElf, Dwarf, Gnome, Draenei, Worgen -> Faction.Alliance
+            Pandaren -> Faction.random()
+            Orc, Troll, Forsaken, Tauren, BloodElf, Goblin -> Faction.Horde
+        }
+
+    companion object {
+        fun random() = values().toList().sample()
+    }
+
     override fun toString() = when (this) {
         Human -> "human"
         NightElf -> "night_elf"
@@ -88,13 +157,22 @@ enum class Race {
 enum class Faction {
     Horde, Alliance;
 
+    companion object {
+        fun random() = values().toList().sample()
+    }
+
     override fun toString() = when (this) {
         Horde -> "horde"
         Alliance -> "alliance"
     }
 }
+
 enum class Gender {
     Female, Male;
+
+    companion object {
+        fun random() = values().toList().sample()
+    }
 
     override fun toString() = when (this) {
         Female -> "female"
@@ -106,30 +184,23 @@ data class Toon(
     val race: Race,
     val wowClass: WowClass,
     val faction: Faction,
-    val gender: Gender
+    val gender: Gender = Gender.random()
 ) {
     override fun toString() = attributes.joinToString(separator = " ")
 
-    private val attributes = if (race == Race.Pandaren)
+    private val attributes = if (race.isMultiFaction)
             listOf(gender, faction, race, wowClass)
         else
             listOf(gender, race, wowClass)
-}
 
-/*
-my %classes_for_race = (
-    human => [qw[warrior paladin hunter rogue monk mage warlock priest]],
-    night_elf => [qw[warrior hunter rogue druid monk mage priest]],
-    dwarf => [qw[warrior paladin hunter shaman rogue monk mage warlock priest]],
-    gnome => [qw[warrior rogue monk mage warlock priest]],
-    draenei => [qw[warrior paladin hunter shaman monk mage priest]],
-    worgen => [qw[warrior hunter druid rogue mage warlock priest]],
-    pandaren => [qw[warrior hunter shaman rogue monk mage priest]],
-    orc => [qw[warrior hunter shaman rogue monk mage warlock]],
-    troll => [qw[warrior hunter shaman druid rogue monk mage warlock priest]],
-    forsaken => [qw[warrior hunter rogue monk mage warlock priest]],
-    tauren => [qw[warrior paladin hunter shaman druid monk priest]],
-    blood_elf => [qw[warrior paladin hunter rogue monk mage warlock priest]],
-    goblin => [qw[warrior hunter shaman rogue mage warlock priest]],
-    );
-*/
+    companion object {
+        fun random(): Toon {
+            val r = Race.random()
+            return Toon(
+                race = r,
+                faction = r.faction,
+                wowClass = WowClass.randomForRace(r)
+            )
+        }
+    }
+}
