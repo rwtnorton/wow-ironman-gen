401f32897549a02b3439aea135fc17afe5aa94c2
Richard W. Norton <rwtnorton@gmail.com>
2026-09-25 02:39:07 -0700

    added tests for f# version

---
+++ fsharp/WowIronmanGen/WowIronmanGen.Tests/DomainTests.fs
@@ -0,0 +1,54 @@
+﻿namespace WowIronmanGen.Tests
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Domain
+
+[<TestClass>]
+type ToonTests () =
+
+    [<TestMethod>]
+    member this.TestInvalidToon () =
+        let toon : Toon = {
+            Race = Tauren
+            WowClass = Hunter
+            Faction = Alliance
+            Gender = Male
+        }
+        Assert.IsFalse(toon.isValid())
+
+    [<TestMethod>]
+    member this.TestIsValidToon () =
+        let toon : Toon = {
+            Race = Tauren
+            WowClass = Hunter
+            Faction = Horde
+            Gender = Male
+        }
+        Assert.IsTrue(toon.isValid())
+
+    [<TestMethod>]
+    member this.TestPandarenToString () =
+        let toon : Toon = {
+            Race = Pandaren
+            WowClass = Shaman
+            Faction = Alliance
+            Gender = Female
+        }
+        Assert.AreEqual("female alliance pandaren shaman", toon.ToString())
+
+    [<TestMethod>]
+    member this.TestNonPandarenToString () =
+        let toon : Toon = {
+            Race = Forsaken
+            WowClass = Rogue
+            Faction = Horde
+            Gender = Female
+        }
+        Assert.AreEqual("female forsaken rogue", toon.ToString())
+
+    [<TestMethod>]
+    member this.TestGeneratedToonIsValid () =
+        let rand = System.Random()
+        let toon = Toon.Generate rand
+        Assert.IsTrue(toon.isValid(), $"generated toon is valid: {toon}")
